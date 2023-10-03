2216e9f97227a4d558b93361c1191fe057c032a2
josephmangara <josephmangara2@gmail.com>
2023-10-03 17:06:26 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function addition(num1,num2){
+    num1 = 1;
+    num2 = 80;
+    return num1 + num2
+}
+
+console.log(addition())
+
+number = 10;
+function add5(){
+    return (number += 5);
+}
+console.log(add5())
+
+function divideBy3(){
+    return (number /= 3);
+}
+console.log(divideBy3())
+
+
+parseInt("2", 10);
+console.log(parseInt())
+
+parseInt("nonsense!", 10);{
+    return 2;
+}
+console.log(parseInt())
+
+function parseFloat(){
+    
+    return 80.123999; 
+}
+console.log(parseFloat())
